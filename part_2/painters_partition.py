83fffd2429e2a837ca4a7ca939be41ccf4f658f5
Chris Rhodes <crr0004@gmail.com>
2019-10-06 13:38:27 +1100

    Fix painters partition python algorithm to work correctly

--- part_2/painters_partition.py
+++ part_2/painters_partition.py
@@ -1,20 +1,18 @@
 def parition(n, k):
-    print("Call part with {} {}".format(n, k))
     if len(n) == 1:
         return n[0]
     if k == 1:
         return sum(n)
 
     a = []
-    for i in range(1, len(n)):
-        b =  [parition(n[i:], k-1), sum(n, i-1)]
-        print(b)
+    for i in range(len(n)):
+        b =  [parition(n[:i+1], k-1), sum(n[i+1:] )]
         a.append( max(
           b 
         ))
     return min(a)
 
 if __name__ == "__main__":
-    input = [1,2,3]#,4,5,6,7,8,9]
+    input = range(10*100*100)#[1,2,3,4,5,6,7,8,9]
     partitions = 2
     print(parition(input, partitions))
